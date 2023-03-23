cb077f1333cbc575171fdc71a1619f810d0eea61
Subhradeep <subhradeeppal30@gmail.com>
2023-03-23 13:47:32 +0530

    sidebar navbar footer responsive done working on body

--- src/static/js/navbarAndSidebar.js
+++ src/static/js/navbarAndSidebar.js
@@ -1,21 +1,39 @@
 document.getElementById("nav-main").insertAdjacentHTML(
   "afterBegin",
-  `<div class="icon-title">
+  /*html*/ `<div class="icon-title">
 
-<img src="./src/static/images/homepage/unnamed.webp" class="nav-icon" alt="" srcset="">
-<span class="nav-title">Youtube Manager</span>
+  <img src="./src/static/images/homepage/unnamed.webp" class="nav-icon" alt="" srcset="">
+  <span class="nav-title">Youtube Manager</span>
 </div>
-<div class="nav-routes" >
-<div class="revenue">Manage Revenue</div>
-<div class="viewers">Manage Viewers</div>
-<div class="videos">Analyze Videos</div>
-<div class="subscribers">Subscribers</div>
+<div class="nav-routes">
+  <div class="revenue">Manage Revenue</div>
+  <div class="viewers">Manage Viewers</div>
+  <div class="videos">Analyze Videos</div>
+  <div class="subscribers">Subscribers</div>
+</div>
+<div class="nav-routes2">
+
+
+  <span class="material-icons">
+    attach_money
+  </span>
+  <span class="material-icons">
+    preview
+  </span>
+  <span class="material-icons">
+    play_circle_filled
+  </span>
+  <span class="material-icons">
+    groups
+  </span>
+
+
 </div>
 <div class="message-icon">
-<span class="material-icons"
-    style="color:#1a73e8;font-size: 2rem;text-shadow: 2px 2px 3px rgb(209, 209, 209);cursor: pointer;">
+  <span class="material-icons"
+    style="color:#1a73e8;text-shadow: 2px 2px 3px rgb(209, 209, 209);cursor: pointer;">
     chat_bubble_outline
-</span>
+  </span>
 </div>`
 );
 [
@@ -27,7 +45,16 @@ document.getElementById("nav-main").insertAdjacentHTML(
     "beforeend",
     `<div>
   <span class="material-icons">
-      ${value.icon}
+    ${value.icon}
+  </span>
+  <h6>${value.text}</h6>
+</div>`
+  );
+  document.getElementById("footer-main").insertAdjacentHTML(
+    "beforeend",
+    `<div>
+  <span class="material-icons">
+    ${value.icon}
   </span>
   <h6>${value.text}</h6>
 </div>`
