6b1af0d13aad0d0e71123be3db235263abfce682
Safiul Haque <mdsafiulhaque4@gmail.com>
2023-03-23 14:57:41 +0530

    comments done

--- src/static/js/navbarAndSidebar.js
+++ src/static/js/navbarAndSidebar.js
@@ -1,21 +1,39 @@
 document.getElementById("nav-main").insertAdjacentHTML(
   "afterBegin",
-  `<div class="icon-title">
+`<div class="icon-title">
 
-<img src="./src/static/images/homepage/unnamed.webp" class="nav-icon" alt="" srcset="">
-<span class="nav-title">Youtube Manager</span>
+  <img src="./src/static/images/homepage/unnamed.webp" class="nav-icon" alt="" srcset="">
+  <span class="nav-title">Youtube Manager</span>
 </div>
-<div class="nav-routes" >
-<div class="revenue">Manage Revenue</div>
-<div class="viewers">Manage Viewers</div>
-<div class="videos">Analyze Videos</div>
-<div class="subscribers">Subscribers</div>
+<div class="nav-routes">
+  <div class="revenue">Manage Revenue</div>
+  <div class="viewers">Manage Viewers</div>
+  <div class="videos">Analyze Videos</div>
+  <div class="subscribers">Subscribers</div>
+</div>
+<div class="nav-routes2">
+
+
+  <span class="material-icons">
+    attach_money
+  </span>
+  <span class="material-icons">
+    preview
+  </span>
+  <span class="material-icons">
+    play_circle_filled
+  </span>
+  <span class="material-icons">
+    groups
+  </span>
+
+
 </div>
 <div class="message-icon">
-<span class="material-icons"
-    style="color:#1a73e8;font-size: 2rem;text-shadow: 2px 2px 3px rgb(209, 209, 209);cursor: pointer;">
+  <span class="material-icons"
+    style="color:#1a73e8;text-shadow: 2px 2px 3px rgb(209, 209, 209);cursor: pointer;">
     chat_bubble_outline
-</span>
+  </span>
 </div>`
 );
 [
@@ -23,13 +41,24 @@ document.getElementById("nav-main").insertAdjacentHTML(
   { icon: "search", text: "Subscribers" },
   { icon: "logout", text: "Logout" },
 ].map((value) => {
-  document.getElementById("sidebar-main").insertAdjacentHTML(
-    "beforeend",
-    `<div>
+  if (document.getElementById("sidebar-main"))
+    document.getElementById("sidebar-main").insertAdjacentHTML(
+      "beforeend",
+      `<div>
   <span class="material-icons">
-      ${value.icon}
+    ${value.icon}
   </span>
   <h6>${value.text}</h6>
 </div>`
-  );
+    );
+  if (document.getElementById("footer-main"))
+    document.getElementById("footer-main").insertAdjacentHTML(
+      "beforeend",
+      `<div>
+  <span class="material-icons">
+    ${value.icon}
+  </span>
+  <h6>${value.text}</h6>
+</div>`
+    );
 });
